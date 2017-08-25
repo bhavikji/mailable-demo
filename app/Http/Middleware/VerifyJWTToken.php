defa68132ea6e12dabb743ec8ddcf8403d8e5374
wmt-php4 <php4.webmob@gmail.com>
2017-08-25 10:23:29 +0530

    initial commit after implementation

---
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Common\ApiResponse;
+use App\Models\User;
+use Closure;
+use Illuminate\Support\Facades\Log;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+
+class VerifyJWTToken
+{
+    use ApiResponse;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = JWTAuth::getToken();
+        if(!$token)
+        {
+            $this->setMeta(__('apiMessages.tokenAbsent'));
+            return response()->json($this->setResponse(),500);
+        }
+        try {
+            $user = $this->auth($token);
+        } catch (TokenExpiredException $e) {
+            $this->setMeta(__('apiMessages.tokenExpired'));
+            return response()->json($this->setResponse(),500);
+        } catch (TokenInvalidException $e) {
+            $this->setMeta(__('apiMessages.tokenMismatch'));
+            return response()->json($this->setResponse(),500);
+        } catch (JWTException $e) {
+            $this->setMeta(__('apiMessages.JWTException'));
+            return response()->json($this->setResponse(), 500);
+        }
+        $request->merge(['user' => $user]);
+        return $next($request);
+    }
+    public function auth($token = false)
+    {
+        $id = JWTAuth::getPayload($token)->get('sub');
+            $user = User::where('id', $id)
+                ->first();
+            if($user){
+                return $user;
+            }
+        return false;
+    }
+}
---
+++ resources/views/mails/hello-world.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+
+        <style>
+            .center {
+                margin: 0 auto;
+                width: 80%;
+            }
+        </style>
+    </head>
+<body>
+<div>
+    <div class="center">
+        <img src="">
+    </div>
+    <div>
+        <span>Dear {{'user'}}</span>
+        <p>Hello World how are you today?</p>
+        <span>Regards,</span>
+        <span>Mailable Team</span>
+    </div>
+</div>
+</body>
+</html>
